9298f38ab8b60fea5d54c4b3eac3c21b95ff3fab
Arbaz Akhter <61103985+Arbaz355@users.noreply.github.com>
2024-09-08 17:50:51 +0530

    Update HashMapWithStingNumber.java

--- HashMap/HashMapWithStingNumber.java
+++ HashMap/HashMapWithStingNumber.java
@@ -3,10 +3,10 @@ import java.util.Collections; //optional API for sorting the element which can b
 public class HashMapWithStingNumber {
     public static void main(String [] args){
         HashMap <String , String> phonebook = new HashMap <String, String> ();
-    phonebook.put("Arbaz akhetr","7004160112");
+    phonebook.put("Arbaz akhter","3434343434");
     phonebook.put("Gulnawaz akhter", "7009166892");
-    phonebook.put("Zarina khatoon", "9234262222");
-    phonebook.put("Tanweer akhter", "9852989898");
+    phonebook.put("Zarina khatoon", "12121212");
+    phonebook.put("Tanweer akhter", "2121212121");
     System.out.println(phonebook.get("Zarina khatoon"));
     for(String i : phonebook.keySet()){
         System.out.println("Name: " +i+ " PhoneNumber: " +phonebook.get(i));
